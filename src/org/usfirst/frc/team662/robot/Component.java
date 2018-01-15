ecb91a9580c62cf01d2968a32990871a1b3e04e6
= <=>
2018-01-15 14:51:29 -0700

    established basics for winch

---
+++ src/org/usfirst/frc/team662/robot/Component.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team662.robot;
+
+public interface Component
+{
+	public void update();
+
+	public void autoUpdate();
+	
+	public void disable();
+}
---
+++ src/org/usfirst/frc/team662/robot/Robot.java
@@ -0,0 +1,6 @@
+package org.usfirst.frc.team662.robot;
+
+public class Robot
+{
+	
+}
